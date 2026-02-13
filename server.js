e517379b8b7a659dd6b9c7f4c7233cbb85d1c85d
Jay-Tntra <jay.parmar@tntra.io>
2026-02-13 11:30:14 +0530

    Day end

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const connectToDB = require('./src/config/db');
 
 
 
+connectToDB();
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
