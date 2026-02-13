5f32b801fe61274ca16f13e06677a9b5d0f6ca16
Jay-Tntra <jay.parmar@tntra.io>
2026-02-13 17:32:17 +0530

    user model created and routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require("express")
+const authRouter = require("./routes/auth.routes")
 
 
 
 const app = express()
+app.use("/api/auth", authRouter)
 
 
-
-module.export = app
+module.exports = app
---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,8 @@
+const userModel = require("../models/user.model")
+
+
+function userRegisterController(req, res){
+
+    const {email,  name , password } = req.body
+    
+}
