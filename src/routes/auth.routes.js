4d30dc9d8dfb1dae25c7248ca7cc1a2fa1d2f157
Jay-Tntra <jay.parmar@tntra.io>
2026-02-16 17:55:46 +0530

    Create controller

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const authController = require("../controllers/auth.controller")
 const router = express.Router();
 
-router.post("/register")
+
+/* POST /api/auth/register */
+router.post("/register", authController.userRegisterController)
 
 
-module.export = router
+module.exports = router
